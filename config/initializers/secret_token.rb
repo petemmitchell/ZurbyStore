7664e305eca2e313dfc813b3cb794251ccc6e63e
Pete Mitchell <petemmitchell@gmail.com>
2014-09-20 14:30:48 -0700

    Initial commit. Probably should have done this a little earlier. Rails cast project up and running.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Store::Application.config.secret_token = 'd6fa56b6c4ad817411b07ccb056715417b5054134d8b1618d5c9ed2fdc5d32c46d34d5075de34f69cb6b046b6900f3aafbc809f7b3d89e81a9f15cfb6a1a4828'
